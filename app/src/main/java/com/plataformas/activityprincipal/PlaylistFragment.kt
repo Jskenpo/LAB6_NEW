8075bdec1a069c1c3211410d936d5c61a806c591
jsantis <san21153@uvg.edu.gt>
2022-08-21 18:33:59 -0600

    cosas importantes terminadas

--- app/src/main/java/com/plataformas/activityprincipal/PlaylistFragment.kt
+++ app/src/main/java/com/plataformas/activityprincipal/PlaylistFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,44 +18,24 @@ private const val ARG_PARAM2 = "param2"
  * Use the [PlaylistFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PlaylistFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class PlaylistFragment : Fragment(R.layout.fragment_playlist) {
+    private lateinit var imageCount: ImageView
+    private lateinit var songs: TextView
+    private var count: Int = 0
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        imageCount = view.findViewById(R.id.Add_Playlist)
+        songs = view.findViewById(R.id.LikedSongs)
+        setListeners()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun setListeners() {
+        imageCount.setOnClickListener {
+            count++
+            songs.text=count.toString()+" Songs"
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_playlist, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PlaylistFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PlaylistFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
